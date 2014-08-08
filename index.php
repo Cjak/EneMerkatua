6f251ede22397c9591f7b932a3c418da77181b9c
Cjak <ifritg@hotmail.fr>
2014-08-08 13:47:12 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Grossiste professionnels et particuliers - Ene Merkatua</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link rel="stylesheet" href="style.css" />
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+		<![endif]-->
+    </head>
+	
+	<body>
+		<header>
+				  <div class="container">
+					<ul class="nav nav-pills nav-justified">
+						<li><img src="images/banniere2.jpg"></img></a></li>
+						<li><a href="index.php" class="white">ACCUEIL</a></li>
+						<li><a href="boutique.php" class="white">NOTRE BOUTIQUE</a></li>
+						<li><a href="#" class="white">NOS PRODUITS</a></li>
+						<li><a href="#" class="white">NOUS CONTACTER <span class="glyphicon glyphicon-envelope"></a></li>
+					</ul>
+				  </div>
+		</header>
+		
+		<!------------------------- LES PRODUITS ----------------------------------------------------------------------------->
+		<div class="container-fluid" id="bloc">
+			<div class="row">			
+				<div class="col-xs-5">
+					<div class="jumbotron">
+						<h1>Bienvenue à Ene Merkatua</h1>
+						<p>Car la famille, c'est bon !</p>
+						<p><a class="btn btn-primary btn-lg" role="button">Voir la boutique</a></p>
+					</div>
+				</div>	
+			</div>
+		</div>
+		
+		<!------------------------- FOOTERS -------------------------------------------------------------------------------------------->					
+		<div class="container-fluid" id="pre-footer">
+			<p class="navbar-text navbar-right">Réalisé par <a href="#" class="navbar-link" style="color:white">La FAMILLE</a></p>
+		</div>
+
+		<footer>
+			<div class="container">
+				<div id="footer-logo"> ENE MERKATUA </div>
+				Ene merkatua  © 2014
+				<div id="footer-separation"></div>
+				<a>
+					<img src="images/drapeau.png"></img>
+				</a>
+			</div>				
+		</footer>			
+	</body>
+</html>
